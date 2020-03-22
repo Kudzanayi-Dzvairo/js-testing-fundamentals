0e483d8051612475a2fc8ec4dfb01ac65b4efe28
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-03-21 23:11:52 -0400

    testing

--- lessons/assertion-library.js
+++ lessons/assertion-library.js
@@ -10,11 +10,11 @@ result = subtract(7, 3)
 expected = 4
 expect(result).toBe(expected)
 
-function expect(actual) {
+function expect(actual){
   return {
-    toBe(expected) {
-      if (actual !== expected) {
-        throw new Error(`${actual} is not equal to ${expected}`)
+    toBe(expected){
+      if(actual !== expected){
+        throw new Error(`${result} is not equal to ${expected}`)
       }
     }
   }
--- lessons/simple.js
+++ lessons/simple.js
@@ -3,13 +3,13 @@ const {sum, subtract} = require('../math')
 let result, expected
 
 result = sum(3, 7)
-expected = 10
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
+expected = 10 
+if(result !== expected){
+    throw new Error (`${result} is not equal to ${expected}`)
 }
 
-result = subtract(7, 3)
+result = subtract(7,3)
 expected = 4
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
+if(result !== expected){
+  throw new Error (`${result} is not equal to ${expected}`)
 }
